ad1e6ca391c4a5f6867474aad627fd6f536ed276
Metas Pattaraamornpan <metasp@ATA-MAC01.local>
2017-09-08 16:43:15 +0700

    -add readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# ATA-IT-Website
+
+## Run locally
+
+```bash
+# clone this repository
+git clone https://github.com/MetasPattaraamornpan/ATA-IT-Website.git
+
+# install lib
+npm install
+
+# build webpack
+npm run build
+
+# run debug frontend
+npm run dev
+
+# run the app
+npm start
+
+```
--- src/app.js
+++ src/app.js
@@ -38,17 +38,3 @@ function start() {
   app.server = server;
   return app;
 }
-
-
-// function start() {
-// var express = require('express');
-//
-// // Create our app
-// var app = express();
-//
-// app.use(express.static('src/client'));
-//
-// app.listen(5000, function () {
-//   console.log('Express server is up on port 3000');
-// });
-// }
