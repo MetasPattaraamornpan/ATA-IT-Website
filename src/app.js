f04caf6f1c16a434cb63faf23dd03ac320fdd933
Metas Pattaraamornpan <metasp@ATA-MAC01.local>
2017-09-08 16:22:41 +0700

    -init project

---
+++ src/app.js
@@ -0,0 +1,54 @@
+const express     = require('express');
+const promise 	  = require('bluebird');
+const http 		    = require('http');
+
+const PORT        = 5000;
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+console.log('NODE_TLS_REJECT_UNAUTHORIZED=0');
+
+//load
+module.exports = () => promise.try(() => preload()).then(start).catch(handleError);
+
+//error
+function handleError(err) {
+    console.log('handleError', err);
+}
+
+//pre start
+function preload() {
+    console.log('load pre-start');
+}
+
+//app start
+function start() {
+  let app = express();
+  let server = http.createServer(app);
+  app.disable('x-powered-by');
+  app.set('trust proxy', true);
+  app.set('port', PORT);
+
+  //route
+  app.use(express.static('src/client'));
+  //start server
+  server.listen(app.get('port') , function() {
+      console.log("Start on port:" + (app.get('port')));
+      return true;
+  });
+  app.server = server;
+  return app;
+}
+
+
+// function start() {
+// var express = require('express');
+//
+// // Create our app
+// var app = express();
+//
+// app.use(express.static('src/client'));
+//
+// app.listen(5000, function () {
+//   console.log('Express server is up on port 3000');
+// });
+// }
