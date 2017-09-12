fd3439b37f57f5a1ed381dd8d71c373d6163949f
SIRIPHA TAKRUDKAEW <Siripha.Takrudkaew@ata-it-th.com>
2017-09-12 17:39:01 +0700

    commit from merge

--- src/client/app/components/footer.jsx
+++ src/client/app/components/footer.jsx
@@ -1,13 +1,19 @@
 import React from "react"
-// import { style } from "../styles/footer.css"
+import { style } from "./styles/footer.css"
 
 class Footer extends React.Component{
   render(){
     return (
-      <footer className="navbar navbar-fixed-bottom">
-        <p className='h4'>Footer</p>
-        <div className="{style.footfont}">Test</div>
-      </footer>
+      <div className="footer">
+        <div className="footerList">Test</div>
+        <div className="social">facebook</div>
+        <div className="social">twitter</div>
+        <div className="social">instagam</div>
+        <footer className="navbar navbar-fixed-bottom">
+          <p className='h4'>Footer</p>
+        </footer>
+      </div>
+
     );
   }
 }
