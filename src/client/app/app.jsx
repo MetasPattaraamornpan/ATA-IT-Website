81a0e774c3bae872cc12180e0e75191310cd886b
Metas Pattaraamornpan <metasp@ATA-MAC01.local>
2017-09-12 17:36:34 +0700

    - add navi bar

--- src/client/app/app.jsx
+++ src/client/app/app.jsx
@@ -5,20 +5,19 @@ import Footer from './components/footer.jsx'
 import Navbar from './components/navbar.jsx'
 //
 // Desktop, tablet and mobile setup
-const Desktop = ({ children }) => <Responsive minWidth={768} children={children} />;
-const Mobile = ({ children }) => <Responsive maxWidth={768} children={children} />;
+const Desktop = ({ children }) => <Responsive minWidth={940} children={children} />;
+const Mobile = ({ children }) => <Responsive maxWidth={940} children={children} />;
 
 export default class App extends React.Component {
   render () {
     return (
-      <div>
         <div>
           <Navbar />
+          <br/><br/><br/><br/><br/><br/><br/><br/><br/>
           <Desktop>You are a desktop or laptop</Desktop>
           <Mobile>You are a mobile phone</Mobile>
           <Footer />
         </div>
-      </div>
     );
   }
 }
