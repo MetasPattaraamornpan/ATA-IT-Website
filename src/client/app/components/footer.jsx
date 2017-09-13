1804b36c3ddaa052c11b8c64c6f036ed062ec473
SIRIPHA TAKRUDKAEW <Siripha.Takrudkaew@ata-it-th.com>
2017-09-13 17:28:32 +0700

    modify footer to support react-flexbox-grid

--- src/client/app/components/footer.jsx
+++ src/client/app/components/footer.jsx
@@ -1,19 +1,49 @@
 import React from "react"
 import { style } from "./styles/footer.css"
+import {Desktop, Mobile } from "./device"
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 class Footer extends React.Component{
   render(){
     return (
-      <div className="footer">
-        <div className="footerList">Test</div>
-        <div className="social">facebook</div>
-        <div className="social">twitter</div>
-        <div className="social">instagam</div>
-        <footer className="navbar navbar-fixed-bottom">
-          <p className='h4'>Footer</p>
-        </footer>
-      </div>
+      <div id="footer">
+        <Desktop>
+          <Row around="xs">
+            <Col xs={1}></Col>
+            <Col xs={4}>
+              <div class="textContainer"><font color="#ffffff">
+                <p class="heading-menu">About Us</p>
+                <p class="heading-menu">Products</p>
+                <ul>
+                  <li class="submenu">Core Banking System</li>
+                  <li class="submenu">Mobile & Web</li>
+                  <li class="submenu">Quality Assurance</li>
+                  <li class="submenu">IT Dashboard</li>
+                  <li class="submenu">IT Security</li>
+                  <li class="submenu">Operation Monitoring </li>
+                </ul>
+                <p class="heading-menu">Careers</p>
+                <ul>
+                  <li class="submenu">Join us</li>
+                </ul>
+                <p class="heading-menu">Contact us</p>
+                <p class="heading-menu"><br/></p>
+                <p class="heading-menu">© ATA IT Limited. All rights reserved 2017.</p>
+                </font>
+              </div>
 
+            </Col>
+            <Col xs={3}></Col>
+            <Col xs={3}>
+              <Row start="xs">
+                
+              </Row>
+            </Col>
+          </Row>
+        </Desktop>
+        <Mobile>
+        </Mobile>
+      </div>
     );
   }
 }
