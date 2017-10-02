18781c6bb33a9b18033a1ae3aefebe67fde89a5f
Nichapa Pakhakorntanatorn <nichapa.pakhakorntanatorn@ata-it-th.com>
2017-10-02 09:44:15 +0700

    react-boilerplate starter

--- src/client/app/components/device.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import Responsive from 'react-responsive';
-
-export const Desktop = ({ children }) => <Responsive minWidth={930} children={children} id="desktop" />;
-export const Mobile = ({ children }) => <Responsive maxWidth={930} children={children} id="mobile"  />;
